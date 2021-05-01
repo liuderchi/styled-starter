5b7d6a10fb46dbe1004ac49963d5b2aba458fe55
Te-Chi Liu <liuderchi@gmail.com>
2021-05-01 22:11:24 +0800

    feat: poc google sheet api
    - https://ithelp.ithome.com.tw/articles/10234325
    - try SSG with gSProps

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,42 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+
 import About from '../src/pages/About'
 import App from '../src/containers/App'
 
-export default () => (
-  <App name="About" prefix="..">
-    <About />
-  </App>
-)
+// TODO env
+import CREDENTIAL from '../.credentials/kl-dev.json'
+import DB from '../.credentials/db.json'
+
+export const getStaticProps = async () => {
+  const doc = new GoogleSpreadsheet(DB.docID)
+  await doc.useServiceAccountAuth(CREDENTIAL)
+  await doc.loadInfo()
+  const sheet = doc.sheetsById[DB.sheetID]
+  const rows = await sheet.getRows()
+
+  let aboutData = {}
+  if (rows?.[0]) {
+    // note: https://theoephraim.github.io/node-google-spreadsheet/#/?id=working-with-rows
+    const { _sheet, _rowNumber, _rawData, ...otherFields } = rows[0]
+    aboutData = otherFields
+  }
+
+  return {
+    props: {
+      about: {
+        ...aboutData,
+      },
+    },
+  }
+}
+
+export default (props) => {
+  return (
+    <App name="About" prefix="..">
+      <>
+        <About />
+        <pre>{JSON.stringify({ props }, null, 2)}</pre>
+      </>
+    </App>
+  )
+}
